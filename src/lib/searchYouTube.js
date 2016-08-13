59583972496370477b05062e8212f64e908704ce
jon-is-learning <jonathan.garrett@gmail.com>
2016-08-13 10:57:04 -0700

    Complete a reusable API helper to query youtube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,19 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  var params = `part=snippet&maxResults=${options.max || 5}&q=${options.query}&type=video&key=${options.key || YOUTUBE_API_KEY}`;
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search?' + params,
+    contentType: 'json',
+    success: function (data) {
+      console.log('success', data.items);
+      callback(data.items);
+    },
+    error: function(error) {
+      console.error('failer', error);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;

c855120ff4b95b22efa2d6646e3322c53c80a121
jon-is-learning <jonathan.garrett@gmail.com>
2016-08-12 17:31:02 -0700

    Complete specs up to click events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,41 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+// var App = (props) => (
+//   <div>
+//     <Nav />
+//     <div className="col-md-7">
+//       <VideoPlayer video={props.videos[0]}/>
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList videos={props.videos}/>
+//     </div>
+//   </div>
+// );
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    console.log(this.props);
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={exampleVideoData[3]}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={exampleVideoData}/>
+        </div>
+      </div>
+    );
+  }
+
+  playVideo (event) {
+    console.log('gonna play the video now ', event);
+    <VideoPlayer video={props.video} />;
+  }
+
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,14 +1,22 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+var VideoListEntry = (props) => {
+
+  var playVideo = (event) => {
+    console.log('gonna play the video now ', event);
+    <VideoPlayer video={props.video} />;
+  };  
+
+  return (
+    <div className="video-list-entry">
+      <div className="media-left media-middle">
+        <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
+      </div>
+      <div className="media-body">
+        <div className="video-list-entry-title" onClick={playVideo}>{props.video.snippet.title}</div>
+        <div className="video-list-entry-detail">{props.video.snippet.description}</div>
+      </div>
     </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated

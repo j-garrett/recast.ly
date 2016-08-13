544c5bf98f19cec8e5e906b70e215f928ae470a5
shea12 <cshearun12@gmail.com>
2016-08-12 23:14:18 -0700

    complete specs through App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,29 @@
-// var App = (props) => (
-//   <div>
-//     <Nav />
-//     <div className="col-md-7">
-//       <VideoPlayer video={props.videos[0]}/>
-//     </div>
-//     <div className="col-md-5">
-//       <VideoList videos={props.videos}/>
-//     </div>
-//   </div>
-// );
-
 class App extends React.Component {
   constructor (props) {
     super(props);
-  var getInitialState = () => {
-    return {currentlyPlaying: props.videos[0],
-      currentList: props.videos};
-  }
-    this.state = getInitialState();
-    // this.state = {
-      // currentlyPlaying: props.videos[0],
-      // currentList: props.videos,
-    //   searchYouTube: props.searchYouTube
 
-    // };
+    this.state = {
+      currentlyPlaying: undefined,
+      currentList: props.searchYouTube,
+    };
   }
 
   render () {
-    console.log(this.props);
+    console.log(this.props);this.state.done ? 'bold' : 'none'
     return (
       <div>
         <Nav />
         <div className="col-md-7">
-          <VideoPlayer video={this.state.currentlyPlaying}/>
+          <VideoPlayer video={this.state.currentlyPlaying ? this.state.currentlyPlaying : exampleVideoData[0]}/>
         </div>
         <div className="col-md-5">
-          <VideoList videos={this.state.currentList} play={this.playVideo.bind(this)}/>
+          <VideoList videos={exampleVideoData} play={this.playVideo.bind(this)}/>
         </div>
       </div>
     );
   }
 
   playVideo (video) {
-    // debugger;
-    console.log('in the app class, gonna play the video now ', this);
-    //render videoplayer with video that was clicked on 
-    //<VideoPlayer video={props.video} />;
     //set the state of the video player with the video that was clicked on
     this.setState({currentlyPlaying: video});
   }
